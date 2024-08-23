68f3b4f5af30f195ccff55b76f128bb208ecf1ca
Ben <ben.d.godfrey2002@gmail.com>
2024-08-23 09:43:22 +0100

    Filtered valid words

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,4 +2,5 @@ require 'open-uri'
 
 file = File.open("data/google-10000-english-no-swears.txt")
 
-DICTIONARY = file.readlines
+#read file, trim string, filter valid words
+DICTIONARY = file.readlines.map{|word| word.chomp}.select{|word| word.length.between?(5,12)}
