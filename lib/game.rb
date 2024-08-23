e286d062aebe2298839b96edf6c209a730fde14a
Ben <ben.d.godfrey2002@gmail.com>
2024-08-23 16:31:14 +0100

    Added #make_guess and #review_guesses

--- lib/game.rb
+++ lib/game.rb
@@ -5,17 +5,49 @@ class Game
     @word = get_random_word
     @guesses_left = 6
     @guesses = []
+    @done = false
+  end
+
+  def play()
+    while @guesses_left > 0 || not(@done)
+      make_guess
+      puts(review_guesses)
+      puts(stickman(6 - @guesses_left))
+    end
   end
 
   def get_guess_input()
     puts("Enter your guess >>")
     input_string = gets.chomp.downcase
     #validate input
-    until DICTIONARY.include?(input_string)
-      puts("Your input is not one of our words. Enter another guess >>")
+    until "abcdefghijklmnopqrstuvwxyz".include?(input_string)
+      puts("Input must be a single letter. Enter another guess >>")
       input_string = gets.chomp.downcase
     end
-    puts(input_string)
+    return input_string
+  end
+
+  def make_guess()
+    guess_input = get_guess_input
+    @guesses.push(guess_input)
+    #reduce guesses_left
+    unless @word.include?(guess_input)
+      @guesses_left -= 1
+    end
+  end
+
+  def review_guesses()
+    @done = true
+    feedback_string = ""
+    @word.each_char do |char|
+      if @guesses.include?(char)
+        feedback_string += char.upcase + " "
+      else
+        feedback_string += "_ "
+        @done = false
+      end
+    end
+    return feedback_string
   end
 
   def get_word
--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@ require_relative("lib/dictionary")
 require_relative("lib/game")
 
 game = Game.new
-game.get_guess_input
+game.play
